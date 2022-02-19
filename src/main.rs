570d7aa734925246f1a30bd745d1d154f9aa9b8e
Adrian <sanctified@local>
2022-02-19 01:33:49 +0100

    Fun with references

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod chapter8 {
+    pub mod game_score {
+
+        use std::collections::HashMap;
+
+        pub fn create_game() -> HashMap<String, i32> {
+            let mut t = HashMap::new();
+            t.insert(String::from("Team Yellow"), 0);
+            t.insert(String::from("Team Blue"), 0);
+            t
+        }
+
+        pub fn show_score(t: &HashMap<String, i32>) {
+            for (key, value) in t {
+                println!("{}: {}", key, value);
+            }
+        }
+
+        pub fn increment_point(k: String, t: &mut HashMap<String, i32>) {
+            let score = t.entry(k).or_insert(0);
+            *score += 1 // dereference score so I can apply a new value to it.
+        }
+    }
+}
+
+use self::chapter8::game_score::{create_game, increment_point, show_score};
+fn main() {
+    let mut x = create_game();
+    show_score(&x);
+    increment_point(String::from("Team Blue"), &mut x);
+    show_score(&x);
+}
